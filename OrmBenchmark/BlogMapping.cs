5e759431e2f7e195484be9e382bc48bf6dc1a6f4
Valerii Gaponov <Valery.GAPONOV@raiffeisen.ru>
2024-05-27 17:46:47 +0300

    add vienna orm

---
+++ OrmBenchmark/BlogMapping.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace OrmBenchmark
+{
+    public class BlogMapping: ClassMap<Blog>
+    {
+        public BlogMapping()
+        {
+            Table("blogs");
+            Id(d => d.Id).Column("Id");
+            Map(d => d.Name).Column("name");
+            Map(d => d.Url).Column("url");
+            Map(d => d.Rating).Column("rating");
+        }
+    }
+}
